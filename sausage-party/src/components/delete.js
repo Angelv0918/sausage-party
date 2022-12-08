3ce9e36a6f4b3e15bc20c6ac8fd3d4277f594a11
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2022-12-08 11:12:51 -0600

    deleteFunction changed to DeleteFunction

--- sausage-party/src/components/delete.js
+++ sausage-party/src/components/delete.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
 
-const deleteFunction = () => {
+const DeleteFunction = () => {
     // =========================
     //           HOOK
     // =========================
@@ -21,4 +21,4 @@ const deleteFunction = () => {
     )
 }
 
-export default deleteFunction
+export default DeleteFunction
