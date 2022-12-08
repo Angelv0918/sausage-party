15c1663aedfff7eb5888ba2683c869bd163f3d94
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2022-12-08 10:41:26 -0600

    mongoose.connection changed to mongoose.connect

--- sausage-party/src/components/delete.js
+++ sausage-party/src/components/delete.js
@@ -1,6 +1,16 @@
-// import react from 'react'
+// import {useState, useEffect} from 'react'
+// import axios from 'axios'
 
 // const deleteFunction = () => {
+
+
+//     const handleDelete = (charData) => {
+//         axios.delete(`http://localhost:3000/sparty/${charData._id}`).then(() => {
+//             axios.get('http://localhost:3000/sparty').then((response) => {
+
+//             })
+//         })
+//     }
 //     return (
 
 //     )
