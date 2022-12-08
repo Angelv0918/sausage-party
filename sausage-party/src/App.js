05ba3c54baaf920a53279c75d02c5ea914aade75
angelv18 <angelvasquez0918@gmail.com>
2022-12-08 10:33:12 -0500

    started frontend

---
+++ sausage-party/src/App.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import './App.css';
+
+const App = () => {
+  const [name, setName] = useState("");
+  const [foodItem, setFoodItem] = useState("");
+  const [Image, setImage] = useState("");
+  const [characters, setCharacters] = useState([]);
+
+
+  
+}
+  
+
+export default App;
